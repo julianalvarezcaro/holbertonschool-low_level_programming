4a30716bd0e194cc00166f50686e1989211f6858
Julián Álvarez <julian.alvarez.caro@hotmail.com>
2020-06-11 12:43:52 -0500

    Trying 2 again

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -11,7 +11,7 @@ int main(void)
 
 	for (alpha = 'a'; alpha <= 'z'; alpha++)
 		putchar(alpha);
-	for (char alpha = 'A'; alpha <= 'Z'; alpha++)
+	for (alpha = 'A'; alpha <= 'Z'; alpha++)
 		putchar(alpha);
 	putchar('\n');
 	return (0);
