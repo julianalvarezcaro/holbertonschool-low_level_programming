583886d023b276569ce0221226a065effcd7291c
Julián Álvarez <julian.alvarez.caro@hotmail.com>
2020-06-10 19:13:47 -0500

    Finished advanced

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+int main(void);
+
+/**
+* main - will display a message
+*
+* Return: 1 always
+*/
+
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return(1);
+}
