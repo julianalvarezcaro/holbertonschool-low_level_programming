c1f31930dee305eeb5541f5fda7174e835d11873
Julián Álvarez <julian.alvarez.caro@hotmail.com>
2020-06-11 15:56:37 -0500

    Added 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main - prints every digit from hexadecimal
+*
+* Returns: 0 always
+*/
+int main(void)
+{
+	int hexa;
+
+	for (hexa = 48; hexa < 58; hexa++)
+		putchar(hexa);
+	for (hexa = 97; hexa < 103; hexa ++)
+		putchar(hexa);
+	putchar('\n');
+	return (0);
+}
