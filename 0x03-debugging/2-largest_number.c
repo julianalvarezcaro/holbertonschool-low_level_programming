7b8546d277c888ee6a67d105c87abe44269d2caa
Julián Álvarez <julian.alvarez.caro@hotmail.com>
2020-06-16 14:28:52 -0500

    Added 2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -24,8 +24,17 @@ int largest_number(int a, int b, int c)
 	{
 		largest = c;
 	}
-	else
+	else if (a == b && a > c)
+	{
 		largest = a;
-
+	}
+	else if (a == c && a > b)
+	{
+		largest = a;
+	}
+	else if (b == c && b > a)
+	{
+		largest = b;
+	}
 	return (largest);
 }
