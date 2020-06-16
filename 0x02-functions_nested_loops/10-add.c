afca152663f24ff82d62bcd7f9f61f39645ca762
Julián Álvarez <julian.alvarez.caro@hotmail.com>
2020-06-16 10:40:40 -0500

    Corrected betty for 10

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,11 +1,12 @@
 #include "holberton.h"
 
 /**
+* add - adds to numbers
 *
+* @a: first integer to be added
+* @b: second integer to be added
 *
-*
-*
-*
+* Return: the addition of a and b
 */
 int add(int a, int b)
 {
