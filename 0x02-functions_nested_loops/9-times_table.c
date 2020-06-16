a7c67073b35741a359b302257d89eb326ecc3a30
Julián Álvarez <julian.alvarez.caro@hotmail.com>
2020-06-16 10:11:34 -0500

    Added 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,40 @@
+#include "holberton.h"
+
+/**
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+void times_table(void)
+{
+	int filas;
+	int colum;
+	int mult;
+	int print1;
+	int print2;
+
+	for(filas = 0; filas < 10; filas++)
+	{
+		for(colum = 0; colum < 10; colum++)
+		{
+			mult = filas * colum;
+			print1 = (mult / 10) % 10;
+			print2 = mult % 10;
+			if(print1 != 0)
+			_putchar(print1 + '0');
+			else if(colum != 0)
+			_putchar(' ');
+			_putchar(print2 + '0');
+
+			if(colum != 9)
+			{
+			_putchar(',');
+			_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
