3f8f6a68b36c1ed72367f1dc3399ae316aadf07b
Julián Álvarez <julian.alvarez.caro@hotmail.com>
2020-06-18 13:43:03 -0500

    Added 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+  * print_diagonal - prints a diagonal across the terminal
+  *
+  * @n: number of lines that it's going to print
+  *
+  * Return: void
+  */
+void print_diagonal(int n)
+{
+	int countLines;
+	int countSpaces;
+
+	if (n != 0)
+	{
+	for (countLines = 0; countLines < n; countLines++)
+	{
+		for (countSpaces = 0; countSpaces < countLines; countSpaces++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
+	}
+	}
+	else
+		_putchar('\n');
+}
