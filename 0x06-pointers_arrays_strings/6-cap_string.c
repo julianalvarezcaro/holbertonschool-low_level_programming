4e5c972fb0705aac12c5484a0d19eb47423f048d
Julián Álvarez <julian.alvarez.caro@hotmail.com>
2020-06-25 19:36:21 -0500

    Corrected 6 again :'v

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,25 +11,19 @@ char *cap_string(char *s)
 {
 	int cap;
 	int len;
-	char sep[13] = {'\t', '\n', ' ', '!', '"', '(', ')',
-		',', '.', ';', '?', '{', '}'};
-	int countSep;
 
 	for (len = 0; s[len] != '\0'; len++)
 	{
-		for (countSep = 0; countSep < 13; countSep++)
-		{
-			if (s[len] == sep[countSep])
-			{
-				cap = 1;
-				break;
-			}
-		}
 		if (len == 0)
 		{
 			if (s[len] >= 97 && s[len] <= 122)
 				s[len] -= 32;
 		}
+		else if (s[len] == 9 ||s[len] == 10 ||s[len] == 32 ||s[len] == 32||
+				s[len] == 34 ||s[len] == 40 ||s[len] == 41 ||
+				s[len] == 44 ||s[len] == 46 ||s[len] == 59 ||
+				s[len] == 63||s[len] == 123 ||s[len] == 125)
+			cap = 1;
 		else if (s[len] >= 97 && s[len] <= 122)
 		{
 			if (cap)
@@ -40,7 +34,7 @@ char *cap_string(char *s)
 		}
 		else if (s[len] >= 65 && s[len] <= 90)
 			cap = 0;
-		else if (s[len] != sep[countSep - 1])
+		else
 			cap = 0;
 
 	}
