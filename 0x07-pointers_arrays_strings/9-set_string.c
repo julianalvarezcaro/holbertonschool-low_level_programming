2c3ceac390166b17ccd9c2048b457d6c3c568c1f
Julián Álvarez <julian.alvarez.caro@hotmail.com>
2020-06-29 21:03:48 -0500

    Added 9

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+  * set_string - sets the value of a pointer
+  *
+  * @s: pointer
+  * @to: pointer
+  *
+  * Return: void
+  */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
