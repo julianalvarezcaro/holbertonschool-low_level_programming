874f901d8bdc5397465ac7597cd8598ba0b6f467
Julián Álvarez <julian.alvarez.caro@hotmail.com>
2020-07-04 22:20:53 -0500

    Completed task 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+
+/**
+  * main - prints addition of 2 numbers
+  *
+  * @argc: number of args
+  * @argv: array of pointers to args
+  *
+  * Return: 0 is succesful, 1 otherwise
+  */
+int main(int argc, char *argv[])
+{
+	int loop;
+	int add = 0;
+
+	if (argc == 1)
+		printf("0\n");
+	else
+	{
+		for (loop = 1; loop < argc; loop++)
+		{
+			if (*argv[loop] < 48 || *argv[loop] > 57)
+			{	
+				printf("Error\n");
+				return (1);
+			}
+			else 
+				add += atoi(argv[loop]);
+		}
+		printf("%d\n", add);
+	}
+	return (0);
+}
