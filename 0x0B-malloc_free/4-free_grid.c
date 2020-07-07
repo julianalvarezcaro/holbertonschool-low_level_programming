bc275bfbaecf557c3245c4537aae8e60b7d21d3e
julianalvarezcaro <julian.alvarez.caro@hotmail.com>
2020-07-07 17:51:53 -0500

    Completed 4

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+  * free_grid - frees memory allocated by malloc
+  *
+  * @grid: grid to be freed
+  * @height: height of the grid
+  *
+  * Return: void
+  *
+  */
+void free_grid(int **grid, int height)
+{
+	int loop;
+	for (loop = 0; loop < height; loop++)
+		free(grid[loop]);
+	free(grid);
+}
