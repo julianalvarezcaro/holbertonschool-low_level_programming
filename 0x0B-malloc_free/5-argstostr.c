b205c158e852220e3fc1071b78682ebafcdbd764
julianalvarezcaro <julian.alvarez.caro@hotmail.com>
2020-07-07 18:44:17 -0500

    Corrected 5

---
+++ 0x0B-malloc_free/5-argstostr.c
@@ -0,0 +1,46 @@
+#include "holberton.h"
+
+/**
+  * argstostr - concatenates all the arguments of the program
+  *
+  * @ac: Number of args
+  * @av: Array of pointers to the args
+  *
+  * Return: Pointer to the new string or NULL if failed
+  */
+char *argstostr(int ac, char **av)
+{
+	char *str;
+	int argc;
+	int argv;
+	int total;
+	int check;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	total = 0;
+	argv = 0;
+	for (argc = 0; argc < ac; argc++)
+	{
+		for (argv = 0; av[argc][argv] != '\0'; argv++)
+		{
+			total++;
+		}
+		total++;
+	}
+	str = malloc(total * sizeof(char));
+	if (str == NULL)
+		return (NULL);
+	check = 0;
+	for (argc = 0; argc < ac; argc++)
+	{
+		for (argv = 0; av[argc][argv] != '\0'; argv++)
+		{
+			str[check] = av[argc][argv];
+			check++;
+		}
+		str[check] = '\n';
+		check++;
+	}
+	return (str);
+}
