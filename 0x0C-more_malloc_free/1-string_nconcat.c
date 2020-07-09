1f92969a64b8453e91ed346b384bd51f99c255bf
julianalvarezcaro <julian.alvarez.caro@hotmail.com>
2020-07-09 13:12:41 -0500

    Corrected lenght case

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -31,7 +31,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (len2 <= n)
 		totalen = len1 + len2 - 1;
 	else
-		totalen = len1 + n - 1;
+		totalen = len1 + n;
 	ptr = malloc(totalen * sizeof(char));
 	if (ptr == NULL)
 		return (NULL);
