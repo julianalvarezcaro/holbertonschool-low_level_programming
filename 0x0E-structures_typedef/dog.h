56cac63742f5cf9bba9e5cfe70d405d7d60e8e19
julianalvarezcaro <julian.alvarez.caro@hotmail.com>
2020-07-13 13:59:20 -0500

    Added description for dog struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,6 +2,12 @@
 #define DOG_H
 
 int _putchar(char c);
+/**
+  * struct dog - Attributes of a dog
+  * @name: it's name
+  * @age: it's age
+  * @owner: name of it's owner
+  */
 struct dog
 {
 	char *name;
