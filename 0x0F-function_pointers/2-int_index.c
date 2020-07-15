fc884faafeb21ea646d7ce76f40a0619510fe772
julianalvarezcaro <julian.alvarez.caro@hotmail.com>
2020-07-15 14:33:59 -0500

    Checking if correct since not sure about the return value in case of null arrary or cmp

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,26 @@
+#include "function_pointers.h"
+
+/**
+  * int_index - finds an int in array
+  *
+  * @array: array in which the elements are in
+  * @size: size of the array
+  * @cmp: compares the element given with the required one
+  *
+  * Return: index of the number found (if so)
+  */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int loop;
+
+	if (array == 0 || cmp == 0)
+		return (1);
+	if (size <= 0)
+		return (-1);
+	for (loop = 0; loop < size; loop++)
+	{
+		if ((*cmp)(array[loop]))
+			return (loop);
+	}
+	return (-1);
+}
