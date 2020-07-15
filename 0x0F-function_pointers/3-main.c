ed32965c2d2dee3b9d593834aa0bcaceeaae7712
julianalvarezcaro <julian.alvarez.caro@hotmail.com>
2020-07-15 16:22:09 -0500

    Chenged returns for exits

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -18,19 +18,19 @@ int main(int argc, char *argv[])
 	if (argc != 4)
 	{
 		printf("Error\n");
-		return (98);
+		exit (98);
 	}
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
 	if (*get_op_func(argv[2]) == NULL)
 	{
 		printf("Error\n");
-		return (99);
+		exit (99);
 	}
 	if ((*argv[2] == '/' || *argv[2] == '%') && b == 0)
 	{
 		printf("Error\n");
-		return (100);
+		exit (100);
 	}
 	result = (*get_op_func(argv[2]))(a, b);
 	printf("%d\n", result);
