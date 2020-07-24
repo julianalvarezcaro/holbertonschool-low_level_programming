505705208a0bba2df91c8cd50d0b04bee804b9df
julianalvarezcaro <julian.alvarez.caro@hotmail.com>
2020-07-23 20:52:02 -0500

    Corrected Betty por task 3

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -10,10 +10,10 @@
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *end;
+	list_t *end, *node;
 	char *cpStr;
 	int lenS;
-	list_t *node;
+	int control = 0;
 
 	end = malloc(sizeof(list_t));
 	end = *head;
@@ -23,41 +23,27 @@ list_t *add_node_end(list_t **head, const char *str)
 		{
 			end = end->next;
 		}
-		/*Just copied to check*/
-		node = malloc(sizeof(list_t));
-		cpStr = strdup(str);
-		if(cpStr == NULL || node == NULL)
-		{
-			free(node);
-			free(cpStr);
-			return (NULL);
-		}
-		for (lenS = 0; str[lenS] != '\0'; lenS++)
-		{
-		}
-		node->next = NULL;
-		end->next = node;
-		node->str = cpStr;
-		node->len = lenS;
-		return (node);
+		control = 1;
 	}
-	else
+	/*Just copied to check*/
+	node = malloc(sizeof(list_t));
+	cpStr = strdup(str);
+	if(cpStr == NULL || node == NULL)
 	{
-		node = malloc(sizeof(list_t));
-		cpStr = strdup(str);
-		if(cpStr == NULL || node == NULL)
-		{
-			free(node);
-			free(cpStr);
-			return (NULL);
-		}
-		for (lenS = 0; str[lenS] != '\0'; lenS++)
-		{
-		}
-		node->next = NULL;
-		*head = node;
-		node->str = cpStr;
-		node->len = lenS;
-		return (node);
+		free(node);
+		free(cpStr);
+		return (NULL);
 	}
+	for (lenS = 0; str[lenS] != '\0'; lenS++)
+	{
+	}
+	node->next = NULL;
+	if (control)
+		end->next = node;
+	else
+		*head = node;
+	node->str = cpStr;
+	node->len = lenS;
+	return (node);
+
 }
