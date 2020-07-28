82dd598c0b0a3ea5049daf7969d6eec34d505648
julianalvarezcaro <julian.alvarez.caro@hotmail.com>
2020-07-28 12:43:55 -0500

    Corrected Betty for task 6

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,22 +1,22 @@
 #include "lists.h"
 
 /**
-  * pop_listint - Deletes the first node of the list
-  *
-  * @head: pointer to the pointer to the first element of the lsit
-  *
-  * Return: the n of the deleted node
-  */
+ * pop_listint - Deletes the first node of the list
+ *
+ * @head: pointer to the pointer to the first element of the lsit
+ *
+ * Return: the n of the deleted node
+ */
 int pop_listint(listint_t **head)
 {
 	listint_t *first;
 	int nVal;
 
-	if(head == NULL || *head == NULL)
+	if (head == NULL || *head == NULL)
 		return (0);
 	first = *head;
 	*head = (*head)->next;
 	nVal = first->n;
 	free(first);
 	return (nVal);
-}	
+}
