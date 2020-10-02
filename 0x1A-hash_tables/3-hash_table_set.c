2650938f15e3d4a4d99568432ab1ea94352f325b
julianalvarezcaro <julian.alvarez.caro@hotmail.com>
2020-10-02 18:45:20 +0000

    Completed task 3

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,37 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - add an element to the hash table
+ *
+ * @ht: hash table to add the element to
+ * @key: given key
+ * @value: value to be added
+ *
+ * Return: 1 in success. 0 otherwise
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *new;
+	hash_node_t *node;
+	char *cp_value;
+	unsigned long int idx;
+
+	if (ht == NULL || key == NULL || strcmp(key, "") || value == NULL)
+		return (0);
+	idx = key_index((const unsigned char *) key, ht->size);
+	new = malloc(sizeof(hash_node_t));
+	if (new == NULL)
+		return (0);
+	cp_value = strdup(value);
+	new->key = (char *)key;
+	new->key = cp_value;
+	if ((ht->array)[idx])
+	{
+		node = (ht->array)[idx];
+		new->next = node;
+		(ht->array)[idx] = new;
+		return (1);
+	}
+	(ht->array)[idx] = new;
+	return (1);
+}
