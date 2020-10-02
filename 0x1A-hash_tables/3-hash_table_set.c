51fde9f1a5e36e18d9b3a91ed4b1dd6ffe92f7e2
julianalvarezcaro <julian.alvarez.caro@hotmail.com>
2020-10-02 23:44:38 +0000

    Deleted validation key emptyy string

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -15,7 +15,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	char *cp_value, *cp_key;
 	unsigned long int idx;
 
-	if (ht == NULL || key == NULL || strcmp(key, "") == 0 || value == NULL)
+	if (ht == NULL || key == NULL || value == NULL)
 		return (0);
 	idx = key_index((const unsigned char *) key, ht->size);
 	cp_value = strdup(value);
