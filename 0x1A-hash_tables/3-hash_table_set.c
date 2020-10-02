a949aa5442c0a2322b17f5e759cd67c91b2a69e0
julianalvarezcaro <julian.alvarez.caro@hotmail.com>
2020-10-02 23:52:17 +0000

    Completed task 3 FINALLY

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -19,8 +19,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	idx = key_index((const unsigned char *) key, ht->size);
 	cp_value = strdup(value);
-	if (cp_value == NULL)
-		return (0);
 	new = malloc(sizeof(hash_node_t));
 	if (new == NULL)
 	{
