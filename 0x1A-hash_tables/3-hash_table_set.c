7a6dbaa987e78b94bfe415d68cb399904c534128
julianalvarezcaro <julian.alvarez.caro@hotmail.com>
2020-10-02 18:47:03 +0000

    Corrected condition if key is an empty string

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -16,7 +16,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	char *cp_value;
 	unsigned long int idx;
 
-	if (ht == NULL || key == NULL || strcmp(key, "") || value == NULL)
+	if (ht == NULL || key == NULL || strcmp(key, "") == 0 || value == NULL)
 		return (0);
 	idx = key_index((const unsigned char *) key, ht->size);
 	new = malloc(sizeof(hash_node_t));
