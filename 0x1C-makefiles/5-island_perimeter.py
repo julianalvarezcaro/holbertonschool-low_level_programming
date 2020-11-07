feef4aeb09284c072970848d595cc9eb7edcc1a5
julianalvarezcaro <julian.alvarez.caro@hotmail.com>
2020-11-07 14:47:12 +0000

    Completed task 5

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+island_perimeter module
+"""
+
+def island_perimeter(grid):
+    """Function that returns the perimeter of an island represented by a grid
+    """
+
+    perimeter = 0
+    for row in range(len(grid)):
+        for col in range(len(grid[row])):
+            here = grid[row][col]
+            up = row - 1
+            down = row + 1
+            left = col - 1
+            right = col + 1
+
+            if here == 0:
+                continue
+            if grid[up][col] == 0:
+                perimeter += 1
+            if grid[down][col] == 0:
+                perimeter += 1
+            if grid[row][left] == 0:
+                perimeter += 1
+            if grid[row][right] == 0:
+                perimeter += 1
+    return perimeter
