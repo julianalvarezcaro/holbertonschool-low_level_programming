0ab36ea2d1008a478794f9438333d00102698eaa
julianalvarezcaro <julian.alvarez.caro@hotmail.com>
2021-02-10 00:07:50 +0000

    Test

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * main - prints the first 50 Fibonacci numbers
+ *
+ * Return: zero
+ */
+int main(void)
+{
+	double suma = 1, a = 1, b;
+	int i;
+
+	for (i = 2; i < 100; i++)
+	{
+		b = suma;
+		suma = suma + a;
+		a = b;
+		printf("%.0f", a);
+		if (i != 99)
+			printf(", ");
+	}
+	printf("\n");
+	return (0);
+}
