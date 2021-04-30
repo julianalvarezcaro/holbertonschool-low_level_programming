fac99dd0741885f7bf8c930c853d47ed4261aca6
julianalvarezcaro <julian.alvarez.caro@hotmail.com>
2021-04-30 11:06:48 -0500

    Testing change

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -14,12 +14,19 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 
 	if (head == NULL)
 		return (NULL);
+
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
+
 	new->prev = NULL;
 	new->next = *head;
 	new->n = n;
+	
+	if (*head != NULL)
+	{
+		(*head)->prev = new;
+	}
 
 	*head = new;
 	return (new);
